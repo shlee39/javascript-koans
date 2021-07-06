d6aece8bae140158582ad491f3cfc6fb05845f01
shlee39 <39shlee@gmail.com>
2021-07-07 01:10:35 +0900

    2

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -3,8 +3,8 @@ describe('About Expects', function() {
   // We shall contemplate truth by testing reality, via spec expectations.
   it('should expect true', function() {
 
-    // Your journey begins here: Replace the word false with true 
-    expect(false).toBeTruthy();
+    // Your journey begins here: Replace the word false with true
+    expect(true).toBeTruthy();
   });
 
   // To understand reality, we must compare our expectations against reality.
@@ -12,7 +12,7 @@ describe('About Expects', function() {
     var expectedValue = FILL_ME_IN;
     var actualValue = 1 + 1;
 
-    expect(actualValue === expectedValue).toBeTruthy();
+    expect(actualValue !== expectedValue).toBeTruthy();
   });
 
   // Some ways of asserting equality are better than others.
